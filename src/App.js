b03ba3a4e50214ca795e0d2b36c9071dc116d344
Joe-kodehode <joe.kodehode@gmail.com>
2022-07-07 21:48:15 +0200

    test

--- src/App.js
+++ src/App.js
@@ -142,44 +142,46 @@ function App() {
 
   return (
     <main>
-      <p>calc</p>
-      <div className="calculator-grid">
+      <div className="calc-wrapper">
+        <p>Calculator Project</p>
         <div className="output">
           <div className="previous-operand">
             {previousOperand} {operation}
           </div>
           <div className="current-operand">{currentOperand}</div>
         </div>
-        <DigitButton digit="7" dispatch={dispatch} />
-        <DigitButton digit="8" dispatch={dispatch} />
-        <DigitButton digit="9" dispatch={dispatch} />
-        <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
-          DEL
-        </button>
-        <DigitButton digit="4" dispatch={dispatch} />
-        <DigitButton digit="5" dispatch={dispatch} />
-        <DigitButton digit="6" dispatch={dispatch} />
-        <OperationButton operation="+" dispatch={dispatch} />
-        <DigitButton digit="1" dispatch={dispatch} />
-        <DigitButton digit="2" dispatch={dispatch} />
-        <DigitButton digit="3" dispatch={dispatch} />
-        <OperationButton operation="-" dispatch={dispatch} />
-        <DigitButton digit="." dispatch={dispatch} />
-        <DigitButton digit="0" dispatch={dispatch} />
-        <OperationButton operation="/" dispatch={dispatch} />
-        <OperationButton operation="*" dispatch={dispatch} />
-        <button
-          className="span-two"
-          onClick={() => dispatch({ type: ACTIONS.CLEAR })}
-        >
-          RESET
-        </button>
-        <button
-          className="span-two"
-          onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
-        >
-          =
-        </button>
+        <div className="calculator-grid">
+          <DigitButton digit="7" dispatch={dispatch} />
+          <DigitButton digit="8" dispatch={dispatch} />
+          <DigitButton digit="9" dispatch={dispatch} />
+          <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+            DEL
+          </button>
+          <DigitButton digit="4" dispatch={dispatch} />
+          <DigitButton digit="5" dispatch={dispatch} />
+          <DigitButton digit="6" dispatch={dispatch} />
+          <OperationButton operation="+" dispatch={dispatch} />
+          <DigitButton digit="1" dispatch={dispatch} />
+          <DigitButton digit="2" dispatch={dispatch} />
+          <DigitButton digit="3" dispatch={dispatch} />
+          <OperationButton operation="-" dispatch={dispatch} />
+          <DigitButton digit="." dispatch={dispatch} />
+          <DigitButton digit="0" dispatch={dispatch} />
+          <OperationButton operation="/" dispatch={dispatch} />
+          <OperationButton operation="*" dispatch={dispatch} />
+          <button
+            className="span-two"
+            onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+          >
+            RESET
+          </button>
+          <button
+            className="span-two"
+            onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+          >
+            =
+          </button>
+        </div>
       </div>
     </main>
   );
